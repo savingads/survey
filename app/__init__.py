fbbb3757b7f9778a9b5c7cc3312db6f96daf79a8
Chris R <70023998+cr625@users.noreply.github.com>
2023-11-10 10:48:09 -0500

    update date comparison

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
+from datetime import datetime
 
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=True, static_folder="static")
     bootstrap = Bootstrap5(app)
     app.config.from_mapping(
         SECRET_KEY="dev",
@@ -31,8 +32,42 @@ def create_app(test_config=None):
     def index():
         return "Index Page!"
 
+    # template filter for datetime
+
+    @app.template_filter("datetime")
+    def format_datetime(value, format="%Y-%m-%d %H:%M"):
+        # convert 20120512093436 to datetime object
+        value = datetime.strptime(value, "%Y%m%d%H%M%S")
+        return value.strftime(format)
+
     @app.route("/screenshots/<path:path>")
     def screenshots(path):
-        return app.send_static_file("screenshots/" + path)
+        # a list of the directory contents of the screenshot folder
+        # screenshot_dir = os.listdir("app/static/screenshots")
+
+        # a list of the directory contents of the screenshot folder plus path
+        screenshot_dir = os.listdir("app/static/screenshots/" + path)
+        # paginate the list
+        screenshot_dir = os.listdir("app/static/screenshots/" + path)[0:20]
+        return render_template("screenshot_list.html", screenshot_dir=screenshot_dir)
+        # return app.send_static_file("screenshots/" + path)
+
+    @app.route("/screenshots/<path:path>/list")
+    def screenshot_list(path):
+        # a list of the directory contents of the screenshot folder
+        screenshot_dir = os.listdir("app/static/screenshots/" + path)[0:20]
+        return render_template("screenshot_display.html", screenshot_dir=screenshot_dir)
+
+    @app.route("/screenshots/<path:path>/display/<filename>")
+    def screenshot_display(path, filename):
+        screenshot_dir = os.listdir("app/static/screenshots/" + path)[0:20]
+
+        selected_screenshot = filename
+        return render_template(
+            "screenshot_display.html",
+            path=path,
+            selected_screenshot=selected_screenshot,
+            screenshot_dir=screenshot_dir,
+        )
 
     return app
